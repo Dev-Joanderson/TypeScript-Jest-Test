8b3dfe9dd0174bd290240eaa8a784a8fadf0a798
joanderson <trajanojoanderson@gmail.com>
2022-06-07 12:26:32 -0300

    first commit

---
+++ src/services/persistency.spec.ts
@@ -0,0 +1,9 @@
+it('descrição do teste (IT)', () => {
+  const number = 1;
+  expect(number).toBe(1);
+});
+
+test('descrição do teste (TEST)', () => {
+  const nome = 'Luiz';
+  expect(nome).toBe('Luiz');
+});
